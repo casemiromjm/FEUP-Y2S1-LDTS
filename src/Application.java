f8e40ea98ceaa6c3885de70ab840fcbcb6a9d0cd
casemiromjm <casemiromelojorge@gmail.com>
2024-09-23 17:13:21 +0100

    updated app

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,7 @@
 public class Application {
     public static void main(String args[]) {
         System.out.println("Hello World");
-        
+        System.out.println("Hello World again");
+        System.out.println("Hello one more time");
     }
 }
